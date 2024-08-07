2a91d9929e7a3e0949fe433bcc2948493f56cac0
Reagan Lowe <reagan@solutionbuilt.com>
2024-08-07 16:33:39 -0400

    end of day updates

---
+++ pages/websites.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../components/Websites.module.css';
+const Websites = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>Websites</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Websites;
